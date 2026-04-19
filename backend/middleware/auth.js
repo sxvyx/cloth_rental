7b8418ad2274b20de23ad474d78ae3961383e9b1
OyeLurker <oyelurker@gmail.com>
2026-04-20 02:23:04 +0530

    + JWT fetchUser (auth routes)

---
+++ backend/middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+const fetchUser = async (req, res, next) => {
+    const token = req.header('auth-token');
+    if (!token) {
+        return res.status(401).send({ errors: "Please authenticate using valid token" });
+    }
+    
+    try {
+        const data = jwt.verify(token, 'secret_ecom');
+        req.user = data.user;
+        next();
+    } catch (error) {
+        res.status(401).send({ errors: "Please authenticate using a valid token" });
+    }
+}
+
+module.exports = fetchUser;
