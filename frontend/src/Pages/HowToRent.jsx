60357fe1efe7f56cbae655210c6e5f6c0992c205
OyeLurker <oyelurker@gmail.com>
2026-04-28 13:45:00 +0530

    added 'How to Rent' guide and supporting static assets

---
+++ frontend/src/Pages/HowToRent.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './CSS/HowToRent.css'
+
+const HowToRent = () => {
+  return (
+    <div className='how-to-rent'>
+      <div className="how-to-rent-container">
+        <h1>How To Rent</h1>
+        <p>Follow these simple steps to rent your favorite outfits from Rentie.</p>
+        <img src="/how_to_rent.jpg" alt="How to rent guide" className="guide-image" />
+      </div>
+    </div>
+  )
+}
+
+export default HowToRent
